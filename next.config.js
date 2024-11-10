36b83f120c7920e6cff4c9e66200fdff4183d9bd
peacebox1218 <peacebox1218@gmail.com>
2024-11-10 11:13:05 +0900

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,12 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  experimental: {
-    // これを追加
-    outputFileTracingIncludes: {
-      '/**': ['./pages/**/*']
-    }
-  }
 }
 
 module.exports = nextConfig
